362362ab14f2f4098657b50cb191e04219ce38b2
ZhetingJin <zheting.jin@yale.edu>
2022-07-20 09:27:02 -0400

    create slab POSCAR from bulk POSCAR
    	modified:   fix_atom.sh
    	new file:   makeslab.sh

---
+++ makeslab.sh
@@ -0,0 +1,8 @@
+echo -e "403\nall\n1\n0 0 0.5" |vaspkit
+python3 ~/project/vasp/VaspTools/POSCAR_direct2cartesian.py POSCAR_REV > POSCAR_cart
+sed -i "s/Direct/Selective \nCart/g" POSCAR_cart
+cp POSCAR_cart POSCAR
+~/project/vasp/VaspTools/fix_atom.sh
+cp POSCAR-new POSCAR
+cp ../4/INCAR .
+
